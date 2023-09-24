da18d0114906d347cbe06ce5dfe9547bfc8c19d0
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-09-24 16:47:16 +0000

    Update public/customersコントローラーのshowアクションのurlを変更

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
     resources :bookmarks,     only:[:create, :index, :destroy]
     resources :workouts,      only:[:new, :create, :edit, :update, :index, :show]
     resources :workout_lists, only:[:create, :index, :show, :destroy]
-    get '/customers/my_page'          => 'customers#show',    as: "customer"
+    resources :customers, only:[:show]
+    # get '/customers/my_page'          => 'customers#show',    as: "customer"
     get '/customers/information/edit' => 'customers#edit',    as: "edit_customer"
     patch '/customers/information'    => 'customers#update',  as: "update_customer"
     get '/customers/unsubscribe', as: "unsubscribe_customer"
