25509268ed0588bccd4fae99a175d6a576377438
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-09-27 02:00:32 +0000

    Update show => whisper に変更、workout を追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,7 +1,10 @@
 class Public::CustomersController < ApplicationController
-  def show
+  def whisper
     @customer = Customer.find(params[:id])
     @whispers = @customer.whispers.all
+  end
+  
+  def workout
   end
 
   def edit
