571a48d9f17901593509c9d1844c0bdb5280ba2e
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-09-27 01:59:08 +0000

    Add ファイル名をshow => whisper に変更、workoutのviewファイルとルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,12 @@ Rails.application.routes.draw do
     resources :bookmarks,     only:[:create, :index, :destroy]
     resources :workouts,      only:[:new, :create, :edit, :update, :index, :show]
     resources :workout_lists, only:[:create, :index, :show, :destroy]
-    resources :customers, only:[:show]
-    # get '/customers/my_page'          => 'customers#show',    as: "customer"
+    # resources :customers, only:[:show]
+    get '/customers/:id/whispers' => 'customers#whisper',         as: "whispers_customer"
+    get '/customers/:id/workouts' => 'customers#workout',         as: "workouts_customer"
     get '/customers/information/edit' => 'customers#edit',    as: "edit_customer"
     patch '/customers/information'    => 'customers#update',  as: "update_customer"
-    get '/customers/unsubscribe', as: "unsubscribe_customer"
+    get '/customers/unsubscribe',                             as: "unsubscribe_customer"
   end
 
   #管理者用
