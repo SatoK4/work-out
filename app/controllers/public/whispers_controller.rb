e3ee6787b098592c786f56b1fd9bea8c04d283bd
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-09-28 08:27:48 +0000

    Add つぶやき新規投稿画面を作成

--- app/controllers/public/whispers_controller.rb
+++ app/controllers/public/whispers_controller.rb
@@ -1,5 +1,15 @@
 class Public::WhispersController < ApplicationController
   def new
+    @whisper = Whisper.new
+  end
+  
+  def create
+    @whisper = Whisper.new(whisper_params)
+    if @whisper.save
+      redirect_to whisper_path(@whisper.id)
+    else
+      render :new
+    end
   end
 
   def index
