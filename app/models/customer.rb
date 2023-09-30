2c08268028c3b7df63724f88c9e8562abcdfa0d4
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-09-30 06:58:44 +0000

    Add 管理者ユーザー一覧画面を作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,13 +3,21 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :workouts
   has_many :workout_comments
   has_many :workout_lists
   has_many :whispers
   has_many :whisper_comments
   has_many :bookmarks
-  
+
   has_one_attached :image
+
+  def status
+    if is_deleted == false
+      "有効"
+    else
+      "無効"
+    end
+  end
 end
