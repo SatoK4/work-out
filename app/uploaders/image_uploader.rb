4e1f89a40533ea863d4257c7de424492639be451
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-10-04 13:22:29 +0000

    Update 表示する画像の大きさを変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,6 +32,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :icon do
     process resize_to_fit: [50, 50]
   end
+  
+  version :profile do
+    process resize_to_fit: [100, 100]
+  end
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
