3c2b3f77c1c1e64cca6d8c2afc97ec0ef30883a8
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-10-18 18:22:02 +0000

    Add tagモデルとアソシエーションを追加

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,8 +2,7 @@ class Workout < ApplicationRecord
   belongs_to :customer
   has_many :workout_comments
   has_many :workout_tags
+  has_many :tags, through: :workout_tags
   has_many :workout_list_details
   has_many :workout_bookmarks
-  
-  mount_uploaders :images, ImageUploader
 end
