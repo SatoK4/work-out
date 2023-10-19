0c7034ebff7ab31716ac9b59176a4cece41f0a48
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-10-19 15:18:16 +0000

    Update tagテーブルのnameカラムをtag_nameに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_13_141139) do
+ActiveRecord::Schema.define(version: 2023_10_19_100821) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2023_10_13_141139) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "tag_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
