f6f83052e778b166efa862fa9f1a7141bddc2680
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-10-27 06:18:54 +0000

    Update ランキングで取得する件数を5件に限定

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @workout_bookmark_ranks = Workout.find(WorkoutBookmark.group(:workout_id).order(Arel.sql('count(workout_id)desc')).pluck(:workout_id))
+    @workout_bookmark_ranks = Workout.find(WorkoutBookmark.group(:workout_id).order(Arel.sql('count(workout_id)desc')).limit(5).pluck(:workout_id))
     # orderの後にArel.sql('count...')を入れるとエラーが別のものになる
   end
 end
