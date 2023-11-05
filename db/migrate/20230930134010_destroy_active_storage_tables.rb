0a7bc2703e2e05089574dded88cfef93e6053e3f
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-11-05 07:47:21 +0000

    change

--- db/migrate/20230930134010_destroy_active_storage_tables.rb
+++ db/migrate/20230930134010_destroy_active_storage_tables.rb
@@ -1,4 +1,5 @@
 class DestroyActiveStorageTables < ActiveRecord::Migration[6.1]
-  drop_table :active_storage_blobs
+  drop_table :active_storage_variant_records
   drop_table :active_storage_attachments
+  drop_table :active_storage_blobs
 end
