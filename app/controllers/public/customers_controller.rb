1393ddc9641119aed3ba730ce82996cce31d8047
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-12-10 05:39:25 +0000

    Add マイページのそれぞれのページにkaminariを適用

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,24 +1,26 @@
 class Public::CustomersController < ApplicationController
   def whisper
     @customer = Customer.find(params[:id])
-    @whispers = @customer.whispers.all
+    @whispers = @customer.whispers.page(params[:page]).per(10)
   end
 
   def whisper_bookmark
     @customer = Customer.find(current_customer.id)
     bookmarks = WhisperBookmark.where(customer_id: current_customer.id).pluck(:whisper_id)
     @whisper_bookmarks = Whisper.find(bookmarks)
+    @whisper_bookmarks = Kaminari.paginate_array(@whisper_bookmarks).page(params[:page]).per(10)
   end
 
   def workout
     @customer = Customer.find(params[:id])
-    @workouts = @customer.workouts.all
+    @workouts = @customer.workouts.page(params[:page]).per(10)
   end
 
   def workout_bookmark
     @customer = Customer.find(params[:id])
     bookmarks = WorkoutBookmark.where(customer_id: current_customer.id).pluck(:workout_id)
     @workout_bookmarks = Workout.find(bookmarks)
+    @workout_bookmarks = Kaminari.paginate_array(@workout_bookmarks).page(params[:page]).per(10)
   end
 
   def edit
